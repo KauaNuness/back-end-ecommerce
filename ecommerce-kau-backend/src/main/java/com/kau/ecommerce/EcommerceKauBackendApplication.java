605412effbdb65188242d28eca00204b84f4e30e
kauanuness <kaua.souza@sptech.school>
2026-04-06 19:21:13 -0300

    chore(setup): inicia projeto ecommerce-kau-backend com spring boot e estrutura de pacotes

---
+++ ecommerce-kau-backend/src/main/java/com/kau/ecommerce/EcommerceKauBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kau.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceKauBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceKauBackendApplication.class, args);
+	}
+
+}
---
+++ ecommerce-kau-backend/src/test/java/com/kau/ecommerce/EcommerceKauBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kau.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceKauBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
